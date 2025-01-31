f317e94cc10470e75850e8e1dde7ac8ff6871fc7
DigbijoySonowal <sonowaldigbijoy51@gmail.com>
2025-01-31 14:36:05 +0530

    amen

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Java
 Learning Java
-This is only to re-learn every concept from scratch.
+-This is only to re-learn every concept from scratch.
---
+++ src/variables/BookTitle.java
@@ -0,0 +1,9 @@
+package variables;
+
+public class BookTitle{
+    public static void main(String[] args) {
+        String bookTitle = "Harry Potter";
+        int pages = 400;
+        System.out.println("My favorite book is " + bookTitle + " which has " + pages + " pages.");
+    }
+}
