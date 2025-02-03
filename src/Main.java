699b8f8c7dade3aeec36f065aaba63f512575dd2
DigbijoySonowal <sonowaldigbijoy51@gmail.com>
2025-02-03 19:22:49 +0530

    amen

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+package variables;
+
+public class Main {
+    static void Mymethod() {
+        System.out.println("This is me.");
+    }
+    public static void main(String[] args) {
+        String name = "Devzy";
+        int age = 12;
+        boolean isSunny = true;
+        System.out.println(name + "\n" + age + "\n" + isSunny);
+        Mymethod();
+    }
+}
